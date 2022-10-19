ce0c395c711eed30e7b34fb62a900dd19cdae484
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-10-19 12:18:00 +0900

    Rename and refactor

--- game/app/main.rb
+++ game/app/main.rb
@@ -12,8 +12,7 @@ def tick(args)
 end
 
 def setup(args)
-  args.state.position = { x: 640, y: 360 }
-  args.state.velocity = { x: 0, y: 0 }
+  args.state.butterfly = { x: 640, y: 360, v_x: 0, v_y: 0 }
   args.state.knife = { x: 640, y: 260, length: 200, angle: 0 }
 end
 
@@ -23,7 +22,7 @@ FLAP_STRENGTH = 4
 
 def process_inputs(inputs, state)
   keyboard = inputs.keyboard
-  state.velocity[:y] += FLAP_STRENGTH if keyboard.key_down.space
+  state.butterfly[:v_y] += FLAP_STRENGTH if keyboard.key_down.space
   state.rotate_knife = keyboard.left_right
 end
 
@@ -31,29 +30,36 @@ def update(state)
   # handle_butterfly_movement(state)
   knife = state.knife
   knife.angle += state.rotate_knife
-  state.knife_bottom = {
+  knife_bottom = {
     x: knife.x - (Math.sin(knife.angle.to_radians) * knife[:length].half),
     y: knife.y + (Math.cos(knife.angle.to_radians) * knife[:length].half)
   }
-  diff_x = state.position.x - state.knife_bottom.x
-  diff_y = state.position.y - state.knife_bottom.y
+  diff_x = state.butterfly[:x] - knife_bottom[:x]
+  diff_y = state.butterfly[:y] - knife_bottom[:y]
   state.knife[:x] += diff_x
   state.knife[:y] += diff_y
+  state.knife_bottom = knife_bottom
 end
 
 def handle_butterfly_movement(state)
-  state.velocity[:y] -= GRAVITY
-  state.velocity[:y] = MAX_VELOCITY if state.velocity[:y] > MAX_VELOCITY
-  state.position[:x] += state.velocity[:x]
-  state.position[:y] += state.velocity[:y]
+  state.butterfly[:v_y] -= GRAVITY
+  state.butterfly[:v_y] = MAX_VELOCITY if state.butterfly[:v_y] > MAX_VELOCITY
+  state.butterfly[:x] += state.butterfly[:v_x]
+  state.butterfly[:y] += state.butterfly[:v_y]
 end
 
 def render(state, outputs)
-  outputs.primitives << {
-    x: state.position.x - 16, y: state.position.y - 16, w: 32, h: 32
-  }.solid!
+  render_butterfly state.butterfly, outputs
+  render_knife state.knife, outputs
+end
 
-  knife = state.knife
+def render_butterfly(butterfly, outputs)
+  outputs.primitives << {
+    x: butterfly[:x] - 16, y: butterfly[:y] - 16, w: 32, h: 32
+  }.solid!
+end
+
+def render_knife(knife, outputs)
   outputs.primitives << {
     x: knife[:x] - 8, y: knife[:y] - knife[:length].half - 8, w: 16, h: knife[:length], angle: knife[:angle],
     path: :pixel
